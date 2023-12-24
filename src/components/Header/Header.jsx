13efbf810ba48d0a91f96f76cffa57e4f9319b2a
Andrew Smith <meuzishun@gmail.com>
2023-12-24 16:50:56 -0500

    Create a constants dir and extract navLinks and refactor Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,9 @@ import PropTypes from 'prop-types';
 import styles from './Header.module.scss';
 import Nav from '../Nav/Nav';
 import Heading from '../Heading/Heading';
+import { preAuthNavLinks, postAuthNavLinks } from '../../constants/navLinks';
 
 function Header({ auth }) {
-  const preAuthNavLinks = [
-    { textContent: 'welcome', url: '#home' },
-    { textContent: 'register', url: '#home' },
-    { textContent: 'login', url: '#home' },
-  ];
-
-  const postAuthNavLinks = [
-    { textContent: 'dashboard', url: '#home' },
-    { textContent: 'profile', url: '#home' },
-    { textContent: 'friends', url: '#home' },
-  ];
-
   return (
     <header className={styles.header}>
       <Heading />
---
+++ src/constants/navLinks.js
@@ -0,0 +1,12 @@
+// navLinks.js
+export const preAuthNavLinks = [
+  { textContent: 'welcome', url: '#home' },
+  { textContent: 'register', url: '#home' },
+  { textContent: 'login', url: '#home' },
+];
+
+export const postAuthNavLinks = [
+  { textContent: 'dashboard', url: '#home' },
+  { textContent: 'profile', url: '#home' },
+  { textContent: 'friends', url: '#home' },
+];
