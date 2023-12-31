eb95b031a09a8d9bcc7a6087b967bcb32cc702b7
Andrew Smith <meuzishun@gmail.com>
2023-12-30 17:11:06 -0800

    Change log to table in localStorage service

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -2,7 +2,7 @@ const storeUserDataAndToken = (data) => {
   const { user, token } = data;
   localStorage.setItem('user', JSON.stringify(user));
   localStorage.setItem('token', JSON.stringify(token));
-  console.log(localStorage);
+  console.table(localStorage);
 };
 
 export { storeUserDataAndToken };
