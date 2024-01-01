4ab5ead0e7f3dec7fe49dbd102fd80f26987a957
Andrew Smith <meuzishun@gmail.com>
2023-12-31 17:24:27 -0800

    Create routes and import necessary components

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,1 +1,25 @@
-export const routes = [];
+import RootLayout from './layouts/RootLayout';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WelcomePage from './pages/WelcomePage';
+
+export const routes = [
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <WelcomePage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+    ],
+  },
+];
