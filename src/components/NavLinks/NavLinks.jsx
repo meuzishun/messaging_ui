b47615f56f24494e050b871e32eac78187aef968
Andrew Smith <meuzishun@gmail.com>
2023-12-31 17:27:21 -0800

    Refactor NavLinks to use react-router-dom NavLink and adjust styles

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,16 +1,15 @@
 import PropTypes from 'prop-types';
 import styles from './NavLinks.module.scss';
-import NavLink from '../NavLink/NavLink';
+// import NavLink from '../NavLink/NavLink';
+import { NavLink } from 'react-router-dom';
 
 function NavLinks({ navLinks, showLinks }) {
   return (
     <ul className={showLinks ? styles.show : styles.hide}>
       {navLinks.map((link, index) => (
-        <NavLink
-          key={`${link.url}-${index}`}
-          textContent={link.textContent}
-          url={link.url}
-        />
+        <li key={`${link.url}-${index}`}>
+          <NavLink to={link.url}>{link.textContent}</NavLink>
+        </li>
       ))}
     </ul>
   );
