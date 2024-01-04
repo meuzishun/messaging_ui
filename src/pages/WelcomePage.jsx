18ee656012fabefd6c4d2540527d56c5f69bb80a
Andrew Smith <meuzishun@gmail.com>
2024-01-04 10:25:31 -0500

    Create WelcomePage and styles

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,5 +1,14 @@
+import Hero from '../components/Hero/Hero';
+import WelcomeNav from '../components/WelcomeNav/WelcomeNav';
+import styles from './WelcomePage.module.scss';
+
 function WelcomePage() {
-  return <div>WelcomePage</div>;
+  return (
+    <div className={styles['welcome-page']}>
+      <Hero />
+      <WelcomeNav />
+    </div>
+  );
 }
 
 export default WelcomePage;
