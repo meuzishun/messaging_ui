1ef69dd6acbd008147692b753be56543fd60ae1b
Andrew Smith <meuzishun@gmail.com>
2024-01-03 19:56:12 -0500

    Wrap heading elements in NavLink from react-router-dom

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,11 +1,14 @@
 import birdPic from '../../assets/img/pigeon.png';
 import styles from './Heading.module.scss';
+import { NavLink } from 'react-router-dom';
 
 function Heading() {
   return (
     <div className={styles.heading}>
-      <img src={birdPic} alt='Picture of pigeon' />
-      <h1>Pigeon</h1>
+      <NavLink to=''>
+        <img src={birdPic} alt='Picture of pigeon' />
+        <h1>Pigeon</h1>
+      </NavLink>
     </div>
   );
 }
