a6f4755d17f4550962ad8e005961032b254fe7a8
Andrew Smith <meuzishun@gmail.com>
2024-01-12 11:57:33 -0500

    Add state to AuthProvider with useReducer

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,15 +1,63 @@
 import PropTypes from 'prop-types';
-import { createContext, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import {
   clearUserDataAndToken,
   getToken,
   setUser,
 } from '../services/localStorage.js';
-import { getProfileWithToken } from '../services/api.js';
+import {
+  postRegisterData,
+  postLoginData,
+  getProfileWithToken,
+} from '../services/api.js';
+
+const INITIALIZE = 'INITIALIZE';
+const REGISTER = 'REGISTER';
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const initialAuthState = {
+  isAuthenticated: false,
+  isInitialized: false,
+  user: null,
+};
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case INITIALIZE:
+      return {
+        isAuthenticated: action.payload.isAuthenticated,
+        isInitialized: true,
+        user: action.payload.user,
+      };
+    case REGISTER:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+    case LOGIN:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
 
 const AuthContext = createContext(null);
 
 function AuthProvider({ children }) {
+  const [authState, dispatch] = useReducer(authReducer, initialAuthState);
+
   const initialize = async () => {
     console.log('Auth initializing...');
     const token = getToken();
@@ -31,19 +79,61 @@ function AuthProvider({ children }) {
     console.log(data);
 
     setUser(data);
+
+    dispatch({
+      type: INITIALIZE,
+      payload: {
+        isAuthenticated: true,
+        user: data,
+      },
+    });
   };
 
-  function register() {
+  const register = async (formData) => {
     console.log('Registering...');
-  }
+    const response = await postRegisterData(formData);
+    const data = await response.json();
 
-  function login() {
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    setUser(data.user);
+
+    dispatch({
+      type: REGISTER,
+      payload: {
+        user: data.user,
+      },
+    });
+  };
+
+  const login = async (formData) => {
     console.log('Logging in...');
-  }
+    const response = await postLoginData(formData);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    setUser(data.user);
+
+    dispatch({
+      type: LOGIN,
+      payload: {
+        user: data.user,
+      },
+    });
+  };
 
   const logout = () => {
     console.log('Logging out...');
     clearUserDataAndToken();
+
+    dispatch({
+      type: LOGOUT,
+    });
   };
 
   useEffect(() => {
@@ -53,6 +143,7 @@ function AuthProvider({ children }) {
   return (
     <AuthContext.Provider
       value={{
+        ...authState,
         register,
         login,
         logout,
