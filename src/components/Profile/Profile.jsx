fa7c92a718d7024ea65ad94f4200628d97d943a7
Andrew Smith <meuzishun@gmail.com>
2024-02-04 14:32:51 -0500

    Create a Profile component and basic styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,32 @@
 import { useState, useEffect } from 'react';
 import ProfileField from '../ProfileField/ProfileField';
+import styles from './Profile.module.scss';
 
 function Profile() {
   const [profile, setProfile] = useState(null);
+  const [edited, setEdited] = useState(false);
 
   const onInputChange = (label, value) => {
     setProfile((prev) => ({ ...prev, [label]: value }));
+    setEdited(true);
   };
 
-  if (profile) {
-    console.log(Object.entries(profile));
-  }
+  const handleSaveClick = () => {
+    // TODO: req editProfile to API... setState on res
+    setEdited(false);
+  };
+
+  const handleUndoClick = () => {
+    // TODO: getProfile from API, setState on res
+    setProfile({
+      name: 'Andrew Smith',
+      email: 'asmith@email.com',
+      title: 'Super awesome guy',
+    });
+  };
 
   useEffect(() => {
+    // TODO: Only on initial render, getProfile from API and set to state
     setProfile({
       name: 'Andrew Smith',
       email: 'asmith@email.com',
@@ -21,7 +35,7 @@ function Profile() {
   }, []);
 
   return (
-    <div>
+    <div className={styles['profile']}>
       {profile
         ? Object.entries(profile).map((arr, i) => (
             <ProfileField
@@ -32,6 +46,12 @@ function Profile() {
             />
           ))
         : null}
+      {edited ? (
+        <div className={styles['btn-container']}>
+          <button onClick={handleSaveClick}>save</button>
+          <button onClick={handleUndoClick}>undo</button>
+        </div>
+      ) : null}
     </div>
   );
 }
