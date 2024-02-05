fd9ab564ff75f79e8c125279acd36e30c288e9ac
Andrew Smith <meuzishun@gmail.com>
2024-02-05 08:15:49 -0500

    Move RootLayout styles to its own file and add overflow-x: hidden for menu animation issue

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import styles from './RootLayout.module.scss';
 
 export default function RootLayout() {
   return (
-    <div className='root-layout'>
+    <div className={styles['root-layout']}>
       <Header auth={false} />
       <main className='main'>
         <Outlet />
