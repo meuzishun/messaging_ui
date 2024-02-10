24279ace6d00ccc9e7e90d40deef57de67d8af22
Andrew Smith <meuzishun@gmail.com>
2024-02-10 13:24:04 -0500

    Refactor ProfileField to accommodate ProfileContext

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,47 +1,12 @@
-import { useState, useEffect } from 'react';
-import { getToken } from '../../services/localStorage';
-import {
-  getProfileWithToken,
-  editProfileWithTokenAndData,
-} from '../../services/api';
+import useProfile from '../../hooks/useProfile';
 import ProfileField from '../ProfileField/ProfileField';
 import Button from '../Button/Button';
 import LoadingMsg from '../LoadingMsg/LoadingMsg';
 import styles from './Profile.module.scss';
 
 function Profile() {
-  const [profile, setProfile] = useState(null);
-  const [edited, setEdited] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onInputChange = (label, value) => {
-    setProfile((prev) => ({ ...prev, [label]: value }));
-    setEdited(true);
-  };
-
-  const handleSaveClick = async () => {
-    const token = getToken();
-    await editProfileWithTokenAndData(token, profile);
-    setEdited(false);
-  };
-
-  const handleUndoClick = () => {
-    initializeProfile();
-    setEdited(false);
-  };
-
-  const initializeProfile = async () => {
-    setIsLoading(true);
-    const token = getToken();
-    const response = await getProfileWithToken(token);
-    const data = await response.json();
-    setProfile(data.user);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    initializeProfile();
-  }, []);
+  const { isLoading, profile, isEdited, saveProfile, revertProfile } =
+    useProfile();
 
   if (isLoading) {
     return <LoadingMsg text='Loading...' />;
@@ -51,27 +16,22 @@ function Profile() {
     <div className={styles['profile']}>
       <h2>Profile</h2>
       {profile
-        ? Object.entries(profile).map((arr, i) => (
-            <ProfileField
-              key={i}
-              label={arr[0]}
-              content={arr[1]}
-              onInputChange={onInputChange}
-            />
+        ? Object.entries(profile).map(([label, content], i) => (
+            <ProfileField key={i} label={label} content={content} />
           ))
         : null}
-      {edited ? (
+      {isEdited ? (
         <div className={styles['btn-container']}>
           <Button
             type='button'
             textContent='save'
-            clickHandler={handleSaveClick}
+            clickHandler={saveProfile}
             classNames={['btn', 'btn-submit']}
           />
           <Button
             type='button'
             textContent='undo'
-            clickHandler={handleUndoClick}
+            clickHandler={revertProfile}
             classNames={['btn']}
           />
         </div>
