1456e3d4f5c1aeaf600d0f41317a2fe9145d14c0
Andrew Smith <meuzishun@gmail.com>
2024-02-12 08:39:43 -0500

    Add LoadingModal to RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import LoadingModal from '../components/LoadingModal/LoadingModal';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import styles from './RootLayout.module.scss';
@@ -6,6 +7,7 @@ import styles from './RootLayout.module.scss';
 export default function RootLayout() {
   return (
     <div className={styles['root-layout']}>
+      <LoadingModal />
       <Header auth={false} />
       <main className='main'>
         <Outlet />
