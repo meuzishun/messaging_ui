eb033206dfa672f337141a5f33c57cd37084f3c8
Andrew Smith <meuzishun@gmail.com>
2024-02-12 13:32:20 -0500

    Add useLoadingModal hook to MessagesContext

--- src/contexts/MessagesContext.jsx
+++ src/contexts/MessagesContext.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { createContext, useState, useEffect } from 'react';
+import useLoadingModal from '../hooks/useLoadingModal';
 import { getToken } from '../services/localStorage';
 import { getMessagesWithToken } from '../services/api';
 
@@ -11,14 +12,17 @@ function MessagesProvider({ children }) {
   const [viewConversation, setViewConversation] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const { setShowLoadingModal } = useLoadingModal();
 
   const getMessages = async () => {
     setIsLoading(true);
+    setShowLoadingModal(true);
     const token = getToken();
     const response = await getMessagesWithToken(token);
     const data = await response.json();
     setConversations(data.messages);
     setIsLoading(false);
+    setShowLoadingModal(false);
   };
 
   useEffect(() => {
