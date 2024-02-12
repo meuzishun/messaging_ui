1b59151cf97c458b8419e9e4c2cab354ee246ed8
Andrew Smith <meuzishun@gmail.com>
2024-02-12 08:40:41 -0500

    Wrap entire App in LoadingModalProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes';
 import { AuthProvider } from './contexts/AuthContext';
+import { LoadingModalProvider } from './contexts/LoadingModalContext';
 
 const router = createBrowserRouter(routes, { basename: '/' });
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <LoadingModalProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </LoadingModalProvider>
     </>
   );
 }
