b59ccd9d5134fcf53f14a37fe19289bfa5a7761a
Andrew Smith <meuzishun@gmail.com>
2024-02-17 15:08:37 -0500

    Add Friend component to Friends component

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,9 +1,24 @@
 import useFriends from '../../hooks/useFriends';
+import Friend from '../Friend/Friend';
 
 function Friends() {
   const { friends } = useFriends();
-  console.log(friends);
-  return <div>Friends</div>;
+
+  if (!friends) {
+    return <div>Loading...</div>;
+  }
+
+  if (friends.length < 1) {
+    return <p>You have no friends</p>;
+  }
+
+  return (
+    <div>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend._id} />
+      ))}
+    </div>
+  );
 }
 
 export default Friends;
