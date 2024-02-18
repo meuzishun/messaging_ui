94f181e69d8a3021a62c1d44db2f974742a0ae6a
Andrew Smith <meuzishun@gmail.com>
2024-02-18 15:53:21 -0500

    Refactor UserSearchContext, replacing useState with useReducer

--- src/contexts/UserSearchContext.jsx
+++ src/contexts/UserSearchContext.jsx
@@ -1,23 +1,78 @@
 import PropTypes from 'prop-types';
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
 import { getToken } from '../services/localStorage.js';
 import { searchUsersWithTokenAndStr } from '../services/api.js';
+
+const LOADING = 'LOADING';
+const RESULTS = 'RESULTS';
+const ERROR = 'ERROR';
+
+const initialUsersState = {
+  isLoading: false,
+  error: null,
+  userSearchResults: [],
+};
+
+const userSearchReducer = (state, action) => {
+  switch (action.type) {
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case RESULTS:
+      return {
+        ...state,
+        loading: false,
+        userSearchResults: action.payload.users,
+      };
+
+    case ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      };
+
+    default:
+      return state;
+  }
+};
 
 const UserSearchContext = createContext(null);
 
 function UserSearchProvider({ children }) {
-  const [userSearchResults, setUserSearchResults] = useState([]);
+  const [state, dispatch] = useReducer(userSearchReducer, initialUsersState);
 
   const searchUsers = async (str) => {
+    if (str.length === 0) {
+      dispatch({
+        type: RESULTS,
+        payload: {
+          users: [],
+        },
+      });
+      return;
+    }
+
+    dispatch({
+      type: LOADING,
+    });
     const token = getToken();
     const response = await searchUsersWithTokenAndStr(token, str);
     const data = await response.json();
 
-    setUserSearchResults(data.users);
+    dispatch({
+      type: RESULTS,
+      payload: {
+        users: data.users,
+      },
+    });
   };
 
   return (
-    <UserSearchContext.Provider value={{ searchUsers, userSearchResults }}>
+    <UserSearchContext.Provider value={{ ...state, searchUsers }}>
       {children}
     </UserSearchContext.Provider>
   );
