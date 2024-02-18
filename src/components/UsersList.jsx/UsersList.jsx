cf090653eb6249948c9beebc1aad3e416fbfba81
Andrew Smith <meuzishun@gmail.com>
2024-02-18 15:58:57 -0500

    Refactor UsersList component to include LoadingMsg and filter results with useFriends and userAuth hooks

--- src/components/UsersList.jsx/UsersList.jsx
+++ src/components/UsersList.jsx/UsersList.jsx
@@ -1,19 +1,31 @@
 import useUserSearch from '../../hooks/useUserSearch';
+import useFriends from '../../hooks/useFriends';
+import useAuth from '../../hooks/useAuth';
+import LoadingMsg from '../LoadingMsg/LoadingMsg';
 import User from '../User/User';
 
 function UsersList() {
-  const { userSearchResults } = useUserSearch();
+  const { user } = useAuth();
+  const { friends } = useFriends();
+  const { isLoading, userSearchResults } = useUserSearch();
 
-  if (userSearchResults.length < 1) {
-    //! We get an error when a space is searched...
-    return null;
+  if (isLoading) {
+    return <LoadingMsg text='loading' />;
   }
 
   return (
     <div>
-      {userSearchResults.map((user) => (
-        <User key={user._id} user={user} />
-      ))}
+      {userSearchResults.length > 1 ? (
+        userSearchResults
+          .filter(
+            ({ _id }) =>
+              !friends.map((friend) => friend._id).includes(_id) &&
+              user._id !== _id
+          )
+          .map((user) => <User key={user._id} user={user} />)
+      ) : (
+        <p>No results</p>
+      )}
     </div>
   );
 }
