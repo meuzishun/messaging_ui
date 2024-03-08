3e657dfc04559054d9868d38b45b4845c1446017
Andrew Smith <meuzishun@gmail.com>
2024-03-08 09:37:15 -0500

    clean up after encapsulating logic in MessagesContext and add a new message btn

--- src/components/ConversationsList/ConversationsList.jsx
+++ src/components/ConversationsList/ConversationsList.jsx
@@ -1,22 +1,11 @@
-import { useEffect } from 'react';
 import useMessages from '../../hooks/useMessages';
-import { formatClassNames } from '../../lib/formatClassNames';
-import { reverseArray } from '../../lib/reverseArray';
 import ConversationPreview from '../ConversationPreview/ConversationPreview';
+import { formatClassNames } from '../../lib/formatClassNames';
 import styles from './ConversationsList.module.scss';
 
 function ConversationsList() {
-  const {
-    conversations,
-    viewConversation,
-    setSelectedConversation,
-    isAnimating,
-    isLoading,
-  } = useMessages();
-
-  const reversedConversations = conversations
-    ? reverseArray(conversations)
-    : null;
+  const { conversations, viewConversation, isLoading, createNewMsg } =
+    useMessages();
 
   const classNames = ['conversations-list'];
 
@@ -24,30 +13,29 @@ function ConversationsList() {
     classNames.push('hide');
   }
 
-  useEffect(() => {
-    if (!isAnimating && !viewConversation) {
-      setSelectedConversation(null);
-    }
-  }, [isAnimating, viewConversation]); //? should this include viewConversation?
-
   if (isLoading) {
     return null;
   }
 
   return (
     <div className={formatClassNames(styles, classNames)}>
-      {!isLoading && conversations?.length < 1 && (
+      <button className={styles['new-msg-btn']} onClick={createNewMsg}>
+        new message
+      </button>
+      {conversations?.length < 1 && (
         <div className={formatClassNames(styles, classNames)}>
           <p className={styles['msg']}>No messages</p>
         </div>
       )}
-      {reversedConversations &&
-        reversedConversations.map((conversation) => (
-          <ConversationPreview
-            key={conversation[0]._id}
-            conversation={conversation}
-          />
-        ))}
+      {conversations &&
+        conversations
+          .reverse()
+          .map((conversation) => (
+            <ConversationPreview
+              key={conversation[0]._id}
+              conversation={conversation}
+            />
+          ))}
     </div>
   );
 }
