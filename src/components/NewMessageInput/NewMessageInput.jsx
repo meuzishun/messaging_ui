31396f62d0ef89dc8b4585641ac6fb8c2e06640e
Andrew Smith <meuzishun@gmail.com>
2024-03-08 09:46:01 -0500

    house new message state in MessagesContext and refactor so more logic is housed in useMessages

--- src/components/NewMessageInput/NewMessageInput.jsx
+++ src/components/NewMessageInput/NewMessageInput.jsx
@@ -1,45 +1,47 @@
-import { useState, useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import useMessages from '../../hooks/useMessages';
-import { getToken } from '../../services/localStorage';
-import { postMessageWithTokenAndData } from '../../services/api';
 import styles from './NewMessageInput.module.scss';
 
 function NewMessageInput() {
-  const [content, setContent] = useState('');
   const inputRef = useRef(null);
-  const { selectedConversation, viewConversation, isAnimating } = useMessages();
-  const { participants, _id: parentId } = selectedConversation.at(-1);
+
+  const {
+    isAnimating,
+    viewConversation,
+    newMessage,
+    editNewMsgContent,
+    sendNewMsg,
+    // printMessagesState,
+  } = useMessages();
+  // printMessagesState();
 
   const handleInput = (e) => {
-    setContent(e.target.value);
+    editNewMsgContent(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const formData = {
-      content,
-      participants,
-      parentId,
-    };
-
-    const token = getToken();
-    postMessageWithTokenAndData(token, formData);
-    setContent('');
+    sendNewMsg();
+    editNewMsgContent('');
+    // printMessagesState();
   };
 
   useEffect(() => {
-    if (!isAnimating && viewConversation) {
+    if (
+      !isAnimating &&
+      viewConversation &&
+      newMessage.participants.length > 1
+    ) {
       inputRef.current.focus();
     }
-  }, [isAnimating]);
+  }, [isAnimating, viewConversation]);
 
   return (
     //? Should we try and use Form component?
     //? What about FormInput and Button components?
     <form className={styles['new-msg-input']} onSubmit={handleSubmit}>
       <input
-        value={content}
+        value={newMessage.content}
         onChange={handleInput}
         type='text'
         ref={inputRef}
