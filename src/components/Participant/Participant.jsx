a726d09e4cb2c31269cb0127e4368dc9722702df
Andrew Smith <meuzishun@gmail.com>
2024-03-08 09:43:26 -0500

    Create Participant component and styles

---
+++ src/components/Participant/Participant.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import useMessages from '../../hooks/useMessages';
+import { BsXCircleFill } from 'react-icons/bs';
+import styles from './Participant.module.scss';
+
+function Participant({ participant }) {
+  const {
+    newMessage: { parentId },
+    removeNewMsgParticipant,
+  } = useMessages();
+
+  const handleRemoveClick = () => {
+    console.log(`Removing ${participant.firstName} ${participant.lastName}`);
+    removeNewMsgParticipant(participant);
+  };
+
+  return (
+    <div className={styles['participant']}>
+      <p className={styles['name']}>
+        {participant.firstName} {participant.lastName}
+      </p>
+
+      {!parentId ? (
+        <div className={styles['icon']} onClick={handleRemoveClick}>
+          <BsXCircleFill />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+Participant.propTypes = {
+  participant: PropTypes.object,
+};
+
+export default Participant;
