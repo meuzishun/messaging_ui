2d4ae6be3a94fbf25369f424aa6b23c073052c90
Andrew Smith <meuzishun@gmail.com>
2024-03-26 14:09:43 -0400

    Refactor routes so guards are only used once as parents

--- src/routes.jsx
+++ src/routes.jsx
@@ -17,72 +17,48 @@ export const routes = [
     element: <RootLayout />,
     children: [
       {
-        index: true,
-        element: (
-          <GuestGuard>
-            <WelcomePage />
-          </GuestGuard>
-        ),
+        path: '',
+        element: <AuthGuard />,
+        children: [
+          {
+            path: 'messages',
+            element: <MessagesPage />,
+          },
+          {
+            path: 'profile',
+            element: <ProfilePage />,
+          },
+          {
+            path: 'friends',
+            element: <FriendsPage />,
+          },
+          {
+            path: 'search',
+            element: <UserSearchPage />,
+          },
+          {
+            path: 'logout',
+            element: <LogoutPage />,
+          },
+        ],
       },
       {
-        path: 'register',
-        element: (
-          <GuestGuard>
-            <RegisterPage />
-          </GuestGuard>
-        ),
-      },
-      {
-        path: 'login',
-        element: (
-          <GuestGuard>
-            <LoginPage />
-          </GuestGuard>
-        ),
-      },
-      {
-        path: 'home',
-        element: (
-          <AuthGuard>
-            <HomePage />
-          </AuthGuard>
-        ),
-      },
-      {
-        path: 'messages',
-        element: (
-          <AuthGuard>
-            <MessagesPage />
-          </AuthGuard>
-        ),
-      },
-      {
-        path: 'profile',
-        element: (
-          <AuthGuard>
-            <ProfilePage />
-          </AuthGuard>
-        ),
-      },
-      {
-        path: 'friends',
-        element: (
-          <AuthGuard>
-            <FriendsPage />
-          </AuthGuard>
-        ),
-      },
-      {
-        path: 'search',
-        element: (
-          <AuthGuard>
-            <UserSearchPage />
-          </AuthGuard>
-        ),
-      },
-      {
-        path: 'logout',
-        element: <LogoutPage />,
+        path: '',
+        element: <GuestGuard />,
+        children: [
+          {
+            index: true,
+            element: <WelcomePage />,
+          },
+          {
+            path: 'register',
+            element: <RegisterPage />,
+          },
+          {
+            path: 'login',
+            element: <LoginPage />,
+          },
+        ],
       },
     ],
   },
