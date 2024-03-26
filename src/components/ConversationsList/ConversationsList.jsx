2cd121f6e9a5d5f5642057c8720cf1a0f4a6eb5f
Andrew Smith <meuzishun@gmail.com>
2024-03-26 19:22:13 -0400

    Remove unnecessary code from ConversationsList

--- src/components/ConversationsList/ConversationsList.jsx
+++ src/components/ConversationsList/ConversationsList.jsx
@@ -1,20 +1,17 @@
 import useMessages from '../../hooks/useMessages';
-import ConversationPreview from '../ConversationPreview/ConversationPreview';
 import LoadingMsg from '../LoadingMsg/LoadingMsg';
-import { formatClassNames } from '../../lib/formatClassNames';
+import ConversationPreview from '../ConversationPreview/ConversationPreview';
 import styles from './ConversationsList.module.scss';
 
 function ConversationsList() {
   const { conversations, isLoading } = useMessages();
-
-  const classNames = ['conversations-list'];
 
   if (isLoading) {
     return <LoadingMsg text='Loading...' />;
   }
 
   return (
-    <div className={formatClassNames(styles, classNames)}>
+    <div className={styles['conversations-list']}>
       {conversations?.length < 1 ? (
         <div>
           <p className={styles['msg']}>No messages</p>
