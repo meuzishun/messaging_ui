99e0c455d8fd69f163b64fb689afd30bc02c0d2b
Andrew Smith <meuzishun@gmail.com>
2024-03-27 08:10:52 -0400

    Clear out unused code in AddSingleParticipant

--- src/components/AddSingleParticipant/AddSingleParticipant.jsx
+++ src/components/AddSingleParticipant/AddSingleParticipant.jsx
@@ -2,13 +2,11 @@ import PropTypes from 'prop-types';
 import useMessages from '../../hooks/useMessages';
 import { BsPlusCircle } from 'react-icons/bs';
 import styles from './AddSingleParticipant.module.scss';
-import { useEffect } from 'react';
 
 function AddSingleParticipant({ participant }) {
-  const { newMessage, addNewMsgParticipant } = useMessages();
+  const { addNewMsgParticipant } = useMessages();
 
   const handleAddParticipant = () => {
-    console.log(`Adding ${participant.firstName} ${participant.lastName}`);
     addNewMsgParticipant(participant);
   };
 
