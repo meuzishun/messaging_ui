b0bc2d2f9004cd0ddd5a13f60be7b8f273e36762
Andrew Smith <meuzishun@gmail.com>
2024-03-27 20:25:37 -0400

    Remove comments from Friends

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,24 +1,11 @@
 import { NavLink } from 'react-router-dom';
-// import useFriends from '../../hooks/useFriends';
-// import Friend from '../Friend/Friend';
 import FriendsList from '../FriendsList/FriendsList';
 import styles from './Friends.module.scss';
 
 function Friends() {
-  // const { friends } = useFriends();
-
-  // if (!friends) {
-  //   return <div>Loading...</div>;
-  // }
-
   return (
     <div className={styles['friends']}>
       <NavLink to={'/search'}>find friend</NavLink>
-      {/* {friends.length > 0 ? (
-        friends.map((friend) => <Friend friend={friend} key={friend._id} />)
-      ) : (
-        <p>You have no friends</p>
-      )} */}
       <FriendsList />
     </div>
   );
