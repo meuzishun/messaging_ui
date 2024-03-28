0185846868f2248ab485ceebadfb22e2022041a0
Andrew Smith <meuzishun@gmail.com>
2024-03-28 14:35:23 -0400

    Fix Header overflow issue by moving LoadingModal up to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes';
 import { AuthProvider } from './contexts/AuthContext';
 import { LoadingModalProvider } from './contexts/LoadingModalContext';
+import LoadingModal from './components/LoadingModal/LoadingModal';
 
 const router = createBrowserRouter(routes, { basename: '/' });
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <LoadingModalProvider>
       <AuthProvider>
+        <LoadingModal />
         <RouterProvider router={router} />
       </AuthProvider>
     </LoadingModalProvider>
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import LoadingModal from '../components/LoadingModal/LoadingModal';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import styles from './RootLayout.module.scss';
@@ -7,7 +6,6 @@ import styles from './RootLayout.module.scss';
 export default function RootLayout() {
   return (
     <div className={styles['root-layout']}>
-      <LoadingModal />
       <Header auth={false} />
       <main className={styles['main']}>
         <Outlet />
