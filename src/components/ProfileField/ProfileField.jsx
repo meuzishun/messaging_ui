e145573f018dade6946d9e350b728593c90259ed
Andrew Smith <meuzishun@gmail.com>
2024-03-30 12:55:37 -0400

    Add useOnClickOutside in ProfileField but use the input element as the ref

--- src/components/ProfileField/ProfileField.jsx
+++ src/components/ProfileField/ProfileField.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
+import { useRef } from 'react';
 import useProfile from '../../hooks/useProfile';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 import { convertCamelcaseToSent } from '../../lib/convertCamelcaseToSent';
 import { BsPencilSquare, BsCheckCircle } from 'react-icons/bs';
 import styles from './ProfileField.module.scss';
 
 function ProfileField({ label, content }) {
   const { setEditField, editProfile, editField } = useProfile();
+  const field = useRef(null);
 
   const handleOkClick = () => {
     setEditField(null);
@@ -19,11 +22,22 @@ function ProfileField({ label, content }) {
     editProfile(label, e.target.value);
   };
 
+  const handleClickOutside = () => {
+    setEditField(null);
+  };
+
+  useOnClickOutside(field, handleClickOutside);
+
   return (
     <div className={styles['profile-field']}>
       <label>{convertCamelcaseToSent(label)}</label>
       {editField === label ? (
-        <input value={content} onChange={handleInputChange} autoFocus />
+        <input
+          value={content}
+          onChange={handleInputChange}
+          autoFocus
+          ref={field}
+        />
       ) : (
         <p>{content}</p>
       )}
