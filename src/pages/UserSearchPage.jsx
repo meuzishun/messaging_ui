cac51fcf9e0e42feaf0b036c1e24a08e09430519
Andrew Smith <meuzishun@gmail.com>
2024-04-02 12:37:56 -0400

    Remove FriendsProvider from UserSearchPage

--- src/pages/UserSearchPage.jsx
+++ src/pages/UserSearchPage.jsx
@@ -1,13 +1,10 @@
 import { UserSearchProvider } from '../contexts/UserSearchContext';
-import { FriendsProvider } from '../contexts/FriendsContext';
 import UserSearch from '../components/UserSearch/UserSearch';
 
 function UserSearchPage() {
   return (
     <UserSearchProvider>
-      <FriendsProvider>
-        <UserSearch />
-      </FriendsProvider>
+      <UserSearch />
     </UserSearchProvider>
   );
 }
