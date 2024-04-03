9f28b202907e9a04d961ee3c401099249bc26485
Andrew Smith <meuzishun@gmail.com>
2024-04-03 19:26:45 -0400

    Fade in ConversationList and LoadingMsg

--- src/components/ConversationsList/ConversationsList.jsx
+++ src/components/ConversationsList/ConversationsList.jsx
@@ -1,33 +1,63 @@
+import { useRef } from 'react';
 import useMessages from '../../hooks/useMessages';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import LoadingMsg from '../LoadingMsg/LoadingMsg';
 import ConversationPreview from '../ConversationPreview/ConversationPreview';
 import styles from './ConversationsList.module.scss';
 
 function ConversationsList() {
+  const loadingRef = useRef(null);
+  const conversationsRef = useRef(null);
   const { conversations, isLoading } = useMessages();
-
-  if (isLoading) {
-    return <LoadingMsg text='Loading...' />;
-  }
+  const nodeRef = isLoading ? loadingRef : conversationsRef;
 
   return (
-    <div className={styles['conversations-list']}>
-      {conversations?.length < 1 ? (
-        <div>
-          <p className={styles['msg']}>No messages</p>
-        </div>
-      ) : (
-        conversations &&
-        conversations
-          .reverse()
-          .map((conversation) => (
-            <ConversationPreview
-              key={conversation[0]._id}
-              conversation={conversation}
-            />
-          ))
-      )}
-    </div>
+    <SwitchTransition mode='out-in'>
+      <CSSTransition
+        key={isLoading}
+        nodeRef={nodeRef}
+        addEndListener={(done) => {
+          nodeRef.current.addEventListener('transitionend', done, false);
+        }}
+        timeout={300}
+        classNames={{
+          appear: styles['fade-appear'],
+          appearActive: styles['fade-appear-active'],
+          appearDone: styles['fade-appear-done'],
+          enter: styles['fade-enter'],
+          enterActive: styles['fade-enter-active'],
+          enterDone: styles['fade-enter-done'],
+          exit: styles['fade-exit'],
+          exitActive: styles['fade-exit-active'],
+          exitDone: styles['fade-exit-done'],
+        }}
+        unmountOnExit
+      >
+        {isLoading ? (
+          <div ref={loadingRef}>
+            <LoadingMsg text='Loading...' />
+          </div>
+        ) : (
+          <div className={styles['conversations-list']} ref={conversationsRef}>
+            {conversations?.length < 1 ? (
+              <div>
+                <p className={styles['msg']}>No messages</p>
+              </div>
+            ) : (
+              conversations &&
+              conversations
+                .reverse()
+                .map((conversation) => (
+                  <ConversationPreview
+                    key={conversation[0]._id}
+                    conversation={conversation}
+                  />
+                ))
+            )}
+          </div>
+        )}
+      </CSSTransition>
+    </SwitchTransition>
   );
 }
 
