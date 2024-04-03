1fcb199d50f81486ee8e032af9d3ee4fc3f029a6
Andrew Smith <meuzishun@gmail.com>
2024-04-03 13:49:10 -0400

    Add nodeRefs to each route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,4 @@
+import { createRef } from 'react';
 import RootLayout from './layouts/RootLayout';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -14,48 +15,59 @@ export const routes = [
   {
     path: '/',
     element: <RootLayout />,
+    nodeRef: createRef(),
     children: [
       {
-        path: '',
+        path: '/',
         element: <GuestGuard />,
+        nodeRef: createRef(),
         children: [
           {
-            index: true,
+            index: '/',
             element: <WelcomePage />,
+            nodeRef: createRef(),
           },
           {
             path: 'register',
             element: <RegisterPage />,
+            nodeRef: createRef(),
           },
           {
             path: 'login',
             element: <LoginPage />,
+            nodeRef: createRef(),
           },
         ],
       },
       {
-        path: '',
+        path: '/',
         element: <AuthGuard />,
+        nodeRef: createRef(),
         children: [
           {
             path: 'messages',
             element: <MessagesPage />,
+            nodeRef: createRef(),
           },
           {
             path: 'profile',
             element: <ProfilePage />,
+            nodeRef: createRef(),
           },
           {
             path: 'friends',
             element: <FriendsPage />,
+            nodeRef: createRef(),
           },
           {
             path: 'search',
             element: <UserSearchPage />,
+            nodeRef: createRef(),
           },
           {
             path: 'logout',
             element: <LogoutPage />,
+            nodeRef: createRef(),
           },
         ],
       },
