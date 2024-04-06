6c9ad173ce4ccdb459a58dcf6ec6e7a9759f8aff
Andrew Smith <meuzishun@gmail.com>
2024-04-06 16:32:10 -0400

    Add FadeTransition component to FriendsList and ConversationsList

--- src/components/ConversationsList/ConversationsList.jsx
+++ src/components/ConversationsList/ConversationsList.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import useMessages from '../../hooks/useMessages';
-import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import FadeTransition from '../FadeTransition/FadeTransition';
 import LoadingMsg from '../LoadingMsg/LoadingMsg';
 import ConversationPreview from '../ConversationPreview/ConversationPreview';
 import styles from './ConversationsList.module.scss';
@@ -12,52 +12,30 @@ function ConversationsList() {
   const nodeRef = isLoading ? loadingRef : conversationsRef;
 
   return (
-    <SwitchTransition mode='out-in'>
-      <CSSTransition
-        key={isLoading}
-        nodeRef={nodeRef}
-        addEndListener={(done) => {
-          nodeRef.current.addEventListener('transitionend', done, false);
-        }}
-        timeout={300}
-        classNames={{
-          appear: styles['fade-appear'],
-          appearActive: styles['fade-appear-active'],
-          appearDone: styles['fade-appear-done'],
-          enter: styles['fade-enter'],
-          enterActive: styles['fade-enter-active'],
-          enterDone: styles['fade-enter-done'],
-          exit: styles['fade-exit'],
-          exitActive: styles['fade-exit-active'],
-          exitDone: styles['fade-exit-done'],
-        }}
-        unmountOnExit
-      >
-        {isLoading ? (
-          <div ref={loadingRef}>
-            <LoadingMsg text='Loading...' />
-          </div>
-        ) : (
-          <div className={styles['conversations-list']} ref={conversationsRef}>
-            {conversations?.length < 1 ? (
-              <div>
-                <p className={styles['msg']}>No messages</p>
-              </div>
-            ) : (
-              conversations &&
-              conversations
-                .reverse()
-                .map((conversation) => (
-                  <ConversationPreview
-                    key={conversation[0]._id}
-                    conversation={conversation}
-                  />
-                ))
-            )}
-          </div>
-        )}
-      </CSSTransition>
-    </SwitchTransition>
+    <FadeTransition parentKey={isLoading} nodeRef={nodeRef} styles={styles}>
+      {isLoading ? (
+        <div ref={loadingRef}>
+          <LoadingMsg text='Loading...' />
+        </div>
+      ) : (
+        <div className={styles['conversations-list']} ref={conversationsRef}>
+          {conversations?.length < 1 ? (
+            <div>
+              <p className={styles['msg']}>No messages</p>
+            </div>
+          ) : (
+            conversations
+              ?.reverse()
+              .map((conversation) => (
+                <ConversationPreview
+                  key={conversation[0]._id}
+                  conversation={conversation}
+                />
+              ))
+          )}
+        </div>
+      )}
+    </FadeTransition>
   );
 }
 
--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import useFriends from '../../hooks/useFriends';
-import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import FadeTransition from '../FadeTransition/FadeTransition';
 import LoadingMsg from '../LoadingMsg/LoadingMsg';
 import Friend from '../Friend/Friend';
 import styles from './FriendsList.module.scss';
@@ -12,46 +12,25 @@ function FriendsList() {
   const nodeRef = isLoading ? loadingRef : friendsRef;
 
   return (
-    <SwitchTransition mode='out-in'>
-      <CSSTransition
-        key={isLoading}
-        nodeRef={nodeRef}
-        addEndListener={(done) => {
-          nodeRef.current.addEventListener('transitionend', done, false);
-        }}
-        timeout={300}
-        classNames={{
-          appear: styles['fade-appear'],
-          appearActive: styles['fade-appear-active'],
-          appearDone: styles['fade-appear-done'],
-          enter: styles['fade-enter'],
-          enterActive: styles['fade-enter-active'],
-          enterDone: styles['fade-enter-done'],
-          exit: styles['fade-exit'],
-          exitActive: styles['fade-exit-active'],
-          exitDone: styles['fade-exit-done'],
-        }}
-        unmountOnExit
-      >
-        {isLoading ? (
-          <div ref={loadingRef}>
-            <LoadingMsg />
-          </div>
-        ) : (
-          <div className={styles['friends-list']} ref={friendsRef}>
-            {friends?.length < 1 ? (
-              <div>
-                <p className={styles['no-friends']}>You have no friends</p>
-              </div>
-            ) : (
-              friends?.map((friend) => (
-                <Friend friend={friend} key={friend._id} />
-              ))
-            )}
-          </div>
-        )}
-      </CSSTransition>
-    </SwitchTransition>
+    <FadeTransition parentKey={isLoading} nodeRef={nodeRef} styles={styles}>
+      {isLoading ? (
+        <div ref={loadingRef}>
+          <LoadingMsg />
+        </div>
+      ) : (
+        <div className={styles['friends-list']} ref={friendsRef}>
+          {friends?.length < 1 ? (
+            <div>
+              <p className={styles['no-friends']}>You have no friends</p>
+            </div>
+          ) : (
+            friends?.map((friend) => (
+              <Friend friend={friend} key={friend._id} />
+            ))
+          )}
+        </div>
+      )}
+    </FadeTransition>
   );
 }
 
