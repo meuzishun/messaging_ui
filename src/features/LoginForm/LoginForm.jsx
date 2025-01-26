62db852d439b0255279f8cb4b2a06e57079a3f1f
Andrew Smith <meuzishun@gmail.com>
2025-01-26 11:28:53 -0500

    Hard code the forms instead of Form and InputForms components

--- src/features/LoginForm/LoginForm.jsx
+++ src/features/LoginForm/LoginForm.jsx
@@ -1,9 +1,9 @@
-import { loginInputFields } from '../../constants/inputFields.js';
+// import { loginInputFields } from '../../constants/inputFields.js';
 import { useState, useRef, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth.jsx';
-import Form from '../../composites/Form/Form.jsx';
-import FormInput from '../../components/FormInput/FormInput.jsx';
+// import Form from '../../composites/Form/Form.jsx';
+// import FormInput from '../../components/FormInput/FormInput.jsx';
 import Button from '../../components/Button/Button.jsx';
 import styles from './LoginForm.module.scss';
 
@@ -12,20 +12,46 @@ function LoginForm() {
   const { login } = useAuth();
   const navigate = useNavigate();
 
-  const inputRefs = {
-    email: useRef(null),
-    password: useRef(null),
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  });
+
+  // const inputRefs = {
+  //   email: useRef(null),
+  //   password: useRef(null),
+  // };
+
+  // const handleSubmit = async (formData) => {
+  //   console.log("LoginForm's formData at top of handleSubmit:");
+  //   console.table(formData);
+  //   passwordRef.current.blur();
+  //   setError(null);
+
+  //   try {
+  //     await login(formData);
+  //     setError(null);
+  //     navigate('/messages');
+  //   } catch (error) {
+  //     setError(error);
+  //   }
+  // };
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = async (formData) => {
-    console.log("LoginForm's formData at top of handleSubmit:");
-    console.table(formData);
-    inputRefs.password.current.blur();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     setError(null);
 
     try {
-      await login(formData);
-      setError(null);
+      await login(formState);
       navigate('/messages');
     } catch (error) {
       setError(error);
@@ -34,22 +60,22 @@ function LoginForm() {
 
   useEffect(() => {
     if (error?.message.includes('email') || error?.message.includes('User')) {
-      inputRefs.email.current.select();
+      emailRef.current.select();
     }
 
     if (error?.message.includes('password')) {
-      inputRefs.password.current.focus();
+      passwordRef.current.focus();
     }
   }, [error]);
 
   useEffect(() => {
-    inputRefs.email.current.focus();
+    emailRef.current.focus();
   }, []);
 
   return (
     <div className={styles['login-form']}>
       <h2>Login</h2>
-      <Form onSubmit={handleSubmit}>
+      {/* <Form onSubmit={handleSubmit}>
         {loginInputFields.map((field) => (
           <FormInput
             key={field.id}
@@ -65,7 +91,40 @@ function LoginForm() {
         <p className={styles['link']}>
           Don&apos;t have an account? <Link to='/register'>Click here</Link>
         </p>
-      </Form>
+      </Form> */}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            value={formState.email}
+            onChange={handleChange}
+            ref={emailRef}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={formState.password}
+            onChange={handleChange}
+            ref={passwordRef}
+          />
+        </fieldset>
+
+        {error && <p>{error.message}</p>}
+
+        <Button type='submit' textContent='Login' classNames={['btn']} />
+
+        <p className={styles['link']}>
+          Don&apos;t have an account? <Link to='/register'>Click here</Link>
+        </p>
+      </form>
     </div>
   );
 }
--- src/features/RegisterForm/RegisterForm.jsx
+++ src/features/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,9 @@
-import { registerInputFields } from '../../constants/inputFields.js';
+// import { registerInputFields } from '../../constants/inputFields.js';
 import { useRef, useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth.jsx';
 import { useNavigate, Link } from 'react-router-dom';
-import Form from '../../composites/Form/Form.jsx';
-import FormInput from '../../components/FormInput/FormInput.jsx';
+// import Form from '../../composites/Form/Form.jsx';
+// import FormInput from '../../components/FormInput/FormInput.jsx';
 import Button from '../../components/Button/Button.jsx';
 import styles from './RegisterForm.module.scss';
 
@@ -12,19 +12,31 @@ function RegisterForm() {
   const { register } = useAuth();
   const navigate = useNavigate();
 
-  const inputRefs = {
-    firstName: useRef(null),
-    lastName: useRef(null),
-    email: useRef(null),
-    password: useRef(null),
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [formState, setFormState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = async (formData) => {
-    inputRefs.password.current.blur();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    passwordRef.current.blur();
     setError(null);
 
     try {
-      await register(formData);
+      await register(formState);
       setError(null);
       navigate('/messages');
     } catch (error) {
@@ -34,22 +46,22 @@ function RegisterForm() {
 
   useEffect(() => {
     if (error?.message.includes('email') || error?.message.includes('User')) {
-      inputRefs.email.current.select();
+      emailRef.current.select();
     }
 
     if (error?.message.includes('password')) {
-      inputRefs.password.current.focus();
+      passwordRef.current.focus();
     }
   }, [error]);
 
   useEffect(() => {
-    inputRefs.firstName.current.focus();
+    firstNameRef.current.focus();
   }, []);
 
   return (
     <div className={styles['register-form']}>
       <h2>Register</h2>
-      <Form onSubmit={handleSubmit}>
+      {/* <Form onSubmit={handleSubmit}>
         {registerInputFields.map((field) => (
           <FormInput
             key={field.id}
@@ -65,7 +77,64 @@ function RegisterForm() {
         <p className={styles['link']}>
           Already have an account? <Link to='/login'>Click here</Link>
         </p>
-      </Form>
+      </Form> */}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor='firstName'>First Name</label>
+          <input
+            type='text'
+            id='firstName'
+            name='firstName'
+            value={formState.firstName}
+            onChange={handleChange}
+            ref={firstNameRef}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='lastName'>Last Name</label>
+          <input
+            type='lastName'
+            id='lastName'
+            name='lastName'
+            value={formState.lastName}
+            onChange={handleChange}
+            ref={lastNameRef}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            value={formState.email}
+            onChange={handleChange}
+            ref={emailRef}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={formState.password}
+            onChange={handleChange}
+            ref={passwordRef}
+          />
+        </fieldset>
+
+        {error && <p>{error.message}</p>}
+
+        <Button type='submit' textContent='Register' classNames={['btn']} />
+
+        <p className={styles['link']}>
+          Already have an account? <Link to='/login'>Click here</Link>
+        </p>
+      </form>
     </div>
   );
 }
